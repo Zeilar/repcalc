8bf200d6c049aa10f995d708e3499553c2c269ab
Philip <philip_angelin@hotmail.com>
2021-08-09 13:17:36 +0200

    Working prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
-import { Route, Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import styled from "styled-components";
+import Home from "./Home";
+import Result from "./Result";
 
 export default function App() {
 	return (
 		<Router>
-			<Route path="/" />
-			<Route path="/result" exact />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/result" exact component={Result} />
+				<Route>404</Route>
+			</Switch>
 		</Router>
 	);
 }
---
+++ src/Home.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+
+export default function Home() {
+	return <div>Home</div>;
+}
